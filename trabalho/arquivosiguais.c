d569b1ce1302e25b4cd13619da830b83868cea6e
André C <andre.correa.silva203@gmail.com>
2022-09-20 22:44:11 -0300

    questão 1 pronta

--- trabalho/arquivosiguais.c
+++ trabalho/arquivosiguais.c
@@ -4,9 +4,46 @@
 int main(int argc, char **argv)
 {
 
-    for (int i = 0; i < argc; i++)
+    unsigned int saida1, saida2;
+    FILE *f1, *f2;
+    char *cont1, *cont2;
+    int flag = 0;
+    cont1 = malloc(sizeof(char) * 300 * 1000000);
+    cont2 = malloc(sizeof(char) * 300 * 1000000);
+    for (int i = 1; i < argc; i++)
     {
-        printf("%s ", argv[i]);
+        f1 = fopen(argv[i], "rb");
+        saida1 = fread(cont1, sizeof(char), 300 * 1000000, f1);
+        for (int j = i + 1; j < argc; j++)
+        {
+            f2 = fopen(argv[j], "rb");
+            saida2 = fread(cont2, sizeof(char), 300 * 1000000, f2);
+            printf("%s %s ", argv[i], argv[j]);
+
+            if (saida1 != saida2)
+            {
+                printf("diferentes\n");
+            }
+            else
+            {
+                flag = 0;
+                for (int k = 0; k < saida1; k++)
+                {
+                    if (cont1[k] != cont2[k])
+                    {
+                        printf("diferentes\n");
+                        flag = 1;
+                        break;
+                    }
+                }
+                if (flag == 0)
+                {
+                    printf("iguais\n");
+                }
+            }
+            fclose(f2);
+        }
+        fclose(f1);
     }
 
     return 0;
