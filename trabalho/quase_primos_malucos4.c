8ab022446d4b9dfa57f279c1f0f5a3ae1770f34e
André C <andre.correa.silva203@gmail.com>
2022-09-23 13:23:07 -0300

    att codigo

--- trabalho/quase_primos_malucos4.c
+++ trabalho/quase_primos_malucos4.c
@@ -10,7 +10,7 @@ typedef struct parametro_thread
     int n;
     int result;
 } parametro_thread;
-
+/*
 int printa(int num, double raiz)
 {
     while (1)
@@ -39,7 +39,7 @@ void *calcula_quase_primo_maluco(void *parameter)
 
     if (param->n <= 508079)
     {
-        param->n = 508079;
+        param->result = 508079;
     }
     else
     {
@@ -49,6 +49,47 @@ void *calcula_quase_primo_maluco(void *parameter)
         }
         double raiz = sqrt((double)param->n);
         param->result = printa(param->n, raiz);
+    }
+}
+*/
+
+int ehquaseprimo(int num, float raiz)
+{
+    if (num % 2 == 0)
+        return 0;
+
+    int cnt = 0;
+    for (int i = 3; i < raiz; i += 2)
+    {
+        if (num % i == 0 && i < 11)
+            return 0;
+        if (num % i == 0 && i > 10)
+            cnt++;
+        if (cnt >= 10)
+            return 1;
+    }
+
+    return 0;
+}
+
+void *quaseprimo(void *arg)
+{
+    parametro_thread *param = arg;
+    int num = (param->n) + 1;
+
+    if (num <= 508079)
+        param->result = 508079;
+    else
+    {
+        while (1)
+        {
+            if (ehquaseprimo(num, floor(sqrt(num))))
+            {
+                param->result = num;
+                break;
+            }
+            num++;
+        }
     }
 }
 
@@ -61,13 +102,14 @@ int main(void)
     parametro_thread PARAMETRO[3];
 
     pthread_t tid[2];
+    /*
     while (1)
     {
         if (scanf(" %d", &PARAMETRO[0].n) == EOF)
         {
             break;
         }
-        pthread_create(&tid[0], NULL, calcula_quase_primo_maluco, &PARAMETRO[0]);
+        pthread_create(&tid[0], NULL, quaseprimo, &PARAMETRO[0]);
         pthread_join(tid[0], NULL);
         printf("%d\n", PARAMETRO[0].result);
 
@@ -76,7 +118,7 @@ int main(void)
             break;
         }
 
-        pthread_create(&tid[1], NULL, calcula_quase_primo_maluco, &PARAMETRO[1]);
+        pthread_create(&tid[1], NULL, quaseprimo, &PARAMETRO[1]);
         pthread_join(tid[1], NULL);
         printf("%d\n", PARAMETRO[1].result);
 
@@ -85,9 +127,59 @@ int main(void)
             break;
         }
 
-        calcula_quase_primo_maluco(&PARAMETRO[2]);
+        quaseprimo(&PARAMETRO[2]);
         printf("%d\n", PARAMETRO[2].result);
     }
+    */
 
+    while (qtd > 0)
+    {
+        if (qtd >= 3)
+        {
+
+            scanf(" %d", &PARAMETRO[0].n);
+
+            pthread_create(&tid[0], NULL, quaseprimo, &PARAMETRO[0]);
+            pthread_join(tid[0], NULL);
+            printf("%d\n", PARAMETRO[0].result);
+
+            scanf(" %d", &PARAMETRO[1].n);
+
+            pthread_create(&tid[1], NULL, quaseprimo, &PARAMETRO[1]);
+            pthread_join(tid[1], NULL);
+            printf("%d\n", PARAMETRO[1].result);
+
+            scanf(" %d", &PARAMETRO[2].n);
+
+            quaseprimo(&PARAMETRO[2]);
+            printf("%d\n", PARAMETRO[2].result);
+            qtd -= 3;
+        }
+        else if (qtd == 2)
+        {
+            scanf(" %d", &PARAMETRO[0].n);
+
+            pthread_create(&tid[0], NULL, quaseprimo, &PARAMETRO[0]);
+            pthread_join(tid[0], NULL);
+            printf("%d\n", PARAMETRO[0].result);
+
+            scanf(" %d", &PARAMETRO[1].n);
+
+            pthread_create(&tid[1], NULL, quaseprimo, &PARAMETRO[1]);
+            pthread_join(tid[1], NULL);
+            printf("%d\n", PARAMETRO[1].result);
+
+            qtd -= 2;
+        }
+        else
+        {
+            scanf(" %d", &PARAMETRO[0].n);
+
+            pthread_create(&tid[0], NULL, quaseprimo, &PARAMETRO[0]);
+            pthread_join(tid[0], NULL);
+            printf("%d\n", PARAMETRO[0].result);
+            qtd--;
+        }
+    }
     return 0;
 }
